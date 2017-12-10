9d92af7122d9c0a89e399fccd4d20773845de497
Lowiz63 <33416152+Lowiz63@users.noreply.github.com>
2017-12-10 19:45:55 +0100

    Tentative de réalisation de la connexion d'un admin

--- vues/acceuil.php
+++ vues/acceuil.php
@@ -5,6 +5,31 @@
  * Date: 08/12/17
  * Time: 19:38
  */
+require_once ("../dal/AdminGateway.php");
+$return = FALSE;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST["pseudo"]))
+        $pseudo = test_input($_POST["pseudo"]);
+    if(isset($_POST["mdp"]))
+        $mdp = test_input($_POST["mdp"]);
+    $gateway= new AdminGateway();
+    $return = $gateway->connexionAdmin($pseudo,$mdp);
+}
+
+
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $data = htmlentities($data);
+    return $data;
+}
+
+
+
 echo '
 <html>
 <head>
@@ -15,14 +40,15 @@ echo '
 <body>
 <header>
     <h1 id="titre">Top10News</h1>
-    <form id="connexion">
+    <h1><?php echo "$return";?></h1>
+    <form action="acceuil.php" method="post" id="connexion">
         <div class="form-group">
             <label >Pseudo</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="pseudo">
+            <input type="text" class="form-control" name="pseudo" aria-describedby="emailHelp" placeholder="pseudo">
         </div>
             <div class="form-group">
             <label for="exampleInputPassword1">Mot de passe</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+            <input type="password" class="form-control" name="mdp" placeholder="Mot de passe">
         </div>
         <button type="submit" class="btn btn-primary">Connexion</button>
     </form>
@@ -86,6 +112,8 @@ echo '
 </footer>
 </body>
 </html>';
+
+
 /*foreach($tabnews as $new){
 print $new->titre;
 print "<br />";
